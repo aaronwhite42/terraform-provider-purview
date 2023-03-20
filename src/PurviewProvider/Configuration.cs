a6cf7ea734696f41699ee86f0aac3e60fd2e2a0e
Aaron <aaron@advocacy.dev>
2023-03-20 10:53:43 +0800

    Rudimentary terraform provider for Microsoft Purview. Implemented Plan and Apply methods

---
+++ src/PurviewProvider/Configuration.cs
@@ -0,0 +1,14 @@
+﻿using MessagePack;
+using System.ComponentModel;
+using TerraformPluginDotNet.Resources;
+
+namespace PurviewProvider;
+
+[MessagePackObject]
+public class Configuration
+{
+    [Key("subscription_id")]
+    [Description("Subscription Id")]
+    [Required]
+    public string? SubscriptionId { get; set; }
+}
